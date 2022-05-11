8ee8347b26b5cf413c0ef76a6211f9314039ee3d
ZeRiix <willflo46@gmail.com>
2022-05-11 08:15:03 +0200

    fixed bugs

--- back/php/includes/header.php
+++ back/php/includes/header.php
@@ -31,17 +31,6 @@ if ($_SESSION['user']["role"] != 'administrateur') {header('Location: ../../../p
                         </li>
                     </ul>
                 </div>
-                <ul class="nav navbar-nav float-right d-flex align-items-center">
-                    <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand"><i class="ficon bx bx-fullscreen"></i></a></li>
-
-                    <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                            <div class="user-nav d-lg-flex d-none"><span class="user-name"><?php echo $_SESSION['user']['name']; ?></span><span class="user-status">Available</span></div><span><img class="round" src="<?php echo $_SESSION['user']["path"];?>" alt="avatar" height="40" width="40"></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right pb-0"><a class="dropdown-item" href="#">
-                                <div class="dropdown-divider mb-0"></div><a class="dropdown-item" href="../../../php/logout.php"><i class="bx bx-power-off mr-50"></i> Logout</a>
-                        </div>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
